9f4af383b7c4df5d15759774cdd6bde86b4363c9
Aleks <aleksey-mazur-7@mail.ru>
2023-08-27 11:39:16 +0300

    Operators

---
+++ src/Main.java
@@ -0,0 +1,71 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*System.out.println(17/3);
+        System.out.println(17%3);
+        System.out.println(3%17);
+
+        System.out.println(17./3);
+        System.out.println(17.%3);
+        System.out.println(3%17.);
+
+        int a = 2;
+        int b = 3;
+        int c = a + b;*/
+
+        /*int i=0;
+        ++i;
+        System.out.println(i);
+        i++;
+        System.out.println(i);
+        --i;
+        System.out.println(i);
+        i--;
+        System.out.println(i);*/
+
+
+        /*i ==0;
+        int j = i++;
+        System.out.println(i);
+        System.out.println(j);*/
+
+        /*int j = ++i;
+        System.out.println(i);
+        System.out.println(j);*/
+
+        /*int a = 2;
+        int b = 3;
+        a = a + b;*/
+
+        /*int a = 2, b = 3;                        //добить из лекции
+        System.out.printf("%d \t %d\n", a, b);
+        //a = a + b;
+        a += b;
+        System.out.printf("%d \t %d\n", a, b);*/
+
+        /*int a,b,c;
+        a = b = c = 7;
+        System.out.printf("%d\t%d\t%d\t\n", a, b, c);*/
+
+        /*System.out.println(!true == false);
+        System.out.println(true && true && false);*/
+        /*int t = 20;
+        boolean rain = false;
+        if(t>10 && !rain);
+        {
+            System.out.println("Идем гулять"); //добить из лекции!!!!!!
+        }
+        else
+        {
+            System.out.println("Сидим дома");*/
+        }
+
+
+
+
+
+
+
+}
